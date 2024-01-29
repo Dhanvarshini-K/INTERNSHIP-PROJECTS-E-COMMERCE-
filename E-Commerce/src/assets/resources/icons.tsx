554be6da3a9ff2f0c8877383341ab5fc43383586
Dhanvarshini <dhanvarshini.k@numentica-ui.com>
2024-01-29 13:50:20 +0530

    changes in icons and image name

--- E-Commerce/src/assets/resources/icons.tsx
+++ E-Commerce/src/assets/resources/icons.tsx
@@ -1,84 +1,84 @@
 //signIn Icons
-export {default as hide_password} from "../icons/hide_password_icon.png"
-export {default as open_password} from "../icons/open_password_icons.png"
+export {default as hide_password} from "../icons/HidePasswordIcon.png"
+export {default as open_password} from "../icons/OpenPasswordIcons.png"
 
 //home Icons
 
-export {default as arrow_right_icon} from "../icons/arrow_right_icon.svg"
+export {default as arrow_right_icon} from "../icons/ArrowRightIcon.svg"
 
 
 //shop Icons
 
-export {default as chevron_right_icon} from "../icons/chevron_right_icon.svg"
-export {default as filter_icon} from "../icons/filter_icon.svg"
-export {default as down_arrow} from "../icons/down_arrow.svg"
-export {default as first_page_first_icon} from "../icons/first_page_first_icon.svg"
-export {default as first_page_second_icon} from "../icons/first_page_second_icon.svg"
-export {default as first_page_third_icon} from "../icons/first_page_third_icon.svg"
-export {default as first_page_fourth_icon} from "../icons/first_page_fourth_icon.svg"
+export {default as chevron_right_icon} from "../icons/ChevronRightIcon.svg"
+export {default as filter_icon} from "../icons/FilterIcon.svg"
+export {default as down_arrow} from "../icons/DownArrow.svg"
+export {default as first_page_first_icon} from "../icons/FirstPageFirstIcon.svg"
+export {default as first_page_second_icon} from "../icons/FirstPageSecondIcon.svg"
+export {default as first_page_third_icon} from "../icons/FirstPageThirdIcon.svg"
+export {default as first_page_fourth_icon} from "../icons/FirstPageFourthIcon.svg"
 
 
 //product Icons
 
-export {default as star} from "../icons/star_icons.svg"
-export {default as heart_icon} from "../icons/heart_icon.svg"
-export {default as addwishlist_icon} from "../icons/addwishlist_icon.svg"
-export {default as wishlist_like} from "../icons/wishlist_like.png"
+export {default as star} from "../icons/StarIcons.svg"
+export {default as heart_icon} from "../icons/HeartIcon.svg"
+export {default as addwishlist_icon} from "../icons/AddWishListIcon.svg"
+export {default as wishlist_like} from "../icons/WishListLike.png"
 
 
 //contact Icons
 
-export {default as address_icons} from "../icons/address_icon.svg"
-export {default as contact_icons} from "../icons/contact_icons.svg"
-export {default as email_icons} from "../icons/email_icon.svg"
+export {default as address_icons} from "../icons/AddressIcon.svg"
+export {default as contact_icons} from "../icons/ContactIcon.svg"
+export {default as email_icons} from "../icons/EmailIcon.svg"
 
 //account Icons
 
-export {default as edit } from "../icons/edit.svg"
-export {default as profile_change} from "../icons/profile_change_icon.svg"
-export {default as user_icon} from "../icons/user_icon.png";
+export {default as edit } from "../icons/Edit.svg"
+export {default as profile_change} from "../icons/ProfileChangeIcon.svg"
+export {default as user_icon} from "../icons/UserIcon.png";
 
 //header Icons
-export {default as ticket_percent} from "../icons/ticket-percent-icon.svg"
-export {default as right_arrow_icon} from "../icons/right_arrow_icon.svg"
-export {default as close_icon} from "../icons/close_icon.svg"
+export {default as ticket_percent} from "../icons/TicketPercentIcon.svg"
+export {default as right_arrow_icon} from "../icons/RightArrowIcon.svg"
+export {default as close_icon} from "../icons/CloseIcon.svg"
 export {default as Logo} from "../icons/Logo.svg"
-export {default as search_icon} from "../icons/search_icon.svg"
-export {default as account_icon} from "../icons/account_icon.svg"
-export {default as cart_icon} from "../icons/cart_icon.svg"
-export {default as wishlist_heart_icon} from "../icons/wishlist_heart_icon.svg"
-export {default as ham_instagram_icon} from "../icons/ham_instagram_icon.svg"
-export {default as ham_facebook_icon} from "../icons/ham_facebook_icon.svg"
-export {default as ham_youtube_icon} from "../icons/ham_youtube_icon.svg"
+export {default as search_icon} from "../icons/SearchIcon.svg"
+export {default as account_icon} from "../icons/AccountIcon.svg"
+export {default as cart_icon} from "../icons/CartIcon.svg"
+export {default as wishlist_heart_icon} from "../icons/WishListHeartIcon.svg"
+export {default as ham_instagram_icon} from "../icons/HamInstagramIcon.svg"
+export {default as ham_facebook_icon} from "../icons/HamFacebookIcon.svg"
+export {default as ham_youtube_icon} from "../icons/HamYoutubeIcon.svg"
 
 
 //footer Icons
 
-export {default as mail_icon} from "../icons/mail_icon.svg"
-export {default as instagram} from "../icons/instagram_icon.svg"
-export {default as facebook} from "../icons/facebook_icon.svg"
-export {default as youtube} from "../icons/youtube_icon.svg"
+export {default as mail_icon} from "../icons/MailIcon.svg"
+export {default as instagram} from "../icons/InstagramIcon.svg"
+export {default as facebook} from "../icons/FacebookIcon.svg"
+export {default as youtube} from "../icons/YoutubeIcon.svg"
 
 
 //serviceCardData Icons
 
-export {default as shipping_icon} from "../icons/shipping_icon.svg"
-export {default as money_back} from "../icons/money_icon.svg"
-export {default as secure_icon } from "../icons/secure_icon.svg"
-export {default as support_icon} from "../icons/support_icon.svg"
+export {default as shipping_icon} from "../icons/ShippingIcon.svg"
+export {default as money_back} from "../icons/MoneyIcon.svg"
+export {default as secure_icon } from "../icons/SecureIcon.svg"
+export {default as support_icon} from "../icons/SupportIcon.svg"
 
 
 
 //cart Icons
 
-export {default as cart_one_dark} from "../icons/cart_one_dark.svg";
-export {default as cart_two_light} from "../icons/cart_two_light.svg"
-export {default as cart_three_light} from "../icons/cart_three_light.svg"
-export {default as paycard} from "../icons/paycard.svg"
-export {default as remove} from "../icons/remove_icon.svg"
-export {default as ticket_percent_light} from "../icons/ticket_percent_light.svg"
+export {default as cart_one_dark} from "../icons/CartOneDark.svg";
+export {default as cart_two_light} from "../icons/CartTwoLight.svg"
+export {default as cart_three_light} from "../icons/CartThreeLight.svg"
+export {default as paycard} from "../icons/PayCard.svg"
+export {default as remove} from "../icons/RemoveIcon.svg"
+export {default as ticket_percent_light} from "../icons/TicketPercentLight.svg"
 
 //article Icons
 
-export {default as article_user} from "../icons/article_user.svg"
-export {default as calendar} from "../icons/calendar.svg"
+export {default as article_user} from "../icons/ArticleUser.svg"
+export {default as calendar} from "../icons/Calendar.svg"
--- E-Commerce/src/assets/resources/images.tsx
+++ E-Commerce/src/assets/resources/images.tsx
@@ -1,75 +1,75 @@
 //signIn Images
-export {default as Sign_img} from "../images/signin_images.svg"
+export {default as Sign_img} from "../images/SignInImage.svg"
 
 //home Images
 
-export {default as slider_images} from "../images/slider_images.svg"
-export {default as livingroom_image} from "../images/livingroom_image.svg"
-export {default as bedroom_images} from "../images/bedroom_images.svg"
-export {default as kitchen_images} from "../images/kitchen_images.svg"
+export {default as slider_images} from "../images/SliderImages.svg"
+export {default as livingroom_image} from "../images/LivingRoomImage.svg"
+export {default as bedroom_images} from "../images/BedRoomImages.svg"
+export {default as kitchen_images} from "../images/KitchenImages.svg"
 
 //product Images
 
-export {default as first_traytable} from "../images/first_traytable_image.svg"
-export {default as second_traytable} from "../images/second_traytable_image.svg"
-export {default as third_traytable} from "../images/third_traytable_image.svg"
-export {default as fourth_traytable} from "../images/fourth_traytable_image.svg"
-export {default as fifth_traytable} from "../images/fifth_traytable_image.svg"
-export {default as sixth_traytable} from "../images/sixth_traytable_image.svg"
-export {default as black_tray} from "../images/black_tray.svg"
-export {default as red_tray} from "../images/red_tray.svg"
-export {default as grey_tray} from "../images/grey_tray.svg"
-export {default as white_tray} from "../images/white_tray.svg"
-export {default as sofia_image} from "../images/sofia_image.svg"
-export {default as nicolas_image} from "../images/nicolas_image.svg"
-export {default as jensen_image} from "../images/jensen_image.svg"
+export {default as first_traytable} from "../images/FirstTrayTableImage.svg"
+export {default as second_traytable} from "../images/SecondTrayTableImage.svg"
+export {default as third_traytable} from "../images/ThirdTrayTableImage.svg"
+export {default as fourth_traytable} from "../images/FourthTrayTableImage.svg"
+export {default as fifth_traytable} from "../images/FifthTrayTableImage.svg"
+export {default as sixth_traytable} from "../images/SixthTrayTableImage.svg"
+export {default as black_tray} from "../images/BlackTray.svg"
+export {default as red_tray} from "../images/RedTray.svg"
+export {default as grey_tray} from "../images/GreyTray.svg"
+export {default as white_tray} from "../images/WhiteTray.svg"
+export {default as sofia_image} from "../images/SofiaImage.svg"
+export {default as nicolas_image} from "../images/NicolasImage.svg"
+export {default as jensen_image} from "../images/JensenImage.svg"
 
 
 //shop Images
 
-export {default as shop_bg_image} from "../images/shop_bg_image.svg"
+export {default as shop_bg_image} from "../images/ShopBgImage.svg"
 
 //footer Images
 
-export {default as footer_logo_image} from "../images/footer_logo_image.svg"
+export {default as footer_logo_image} from "../images/FooterLogoImage.svg"
 
 
 //articleData Images
 
-export {default as home_image} from "../images/article_home_image.svg"
-export {default as kitchen_image} from "../images/article_kitchen_image.svg"
-export {default as bedroom_image} from "../images/article_bedroom_image.svg"
-export {default as article1} from "../images/article1_image.svg"
-export {default as article2} from "../images/article2_image.svg"
-export {default as article3} from "../images/article3_image.svg"
-export {default as article4} from "../images/article4_image.svg"
-export {default as article5} from "../images/article5_image.svg"
-export {default as article6} from "../images/article6_image.svg"
+export {default as home_image} from "../images/ArticleHomeImage.svg"
+export {default as kitchen_image} from "../images/ArticleKitchenImage.svg"
+export {default as bedroom_image} from "../images/ArticleBedroomImage.svg"
+export {default as article1} from "../images/Article1Image.svg"
+export {default as article2} from "../images/Article2Image.svg"
+export {default as article3} from "../images/Article3Image.svg"
+export {default as article4} from "../images/Article4Image.svg"
+export {default as article5} from "../images/Article5Image.svg"
+export {default as article6} from "../images/Article6Image.svg"
 
 
 //productData Images
 
-export {default as sofaImages} from "../images/sofa_images.svg"
-export {default as luxuaryImages} from "../images/luxuary_sofa_images.svg"
-export {default as tableLamp} from "../images/tablelamp_image.svg"
-export {default as cozySofa} from "../images/cozy_sofa_images.svg"
-export {default as drawer} from "../images/drawer_images.svg"
-export {default as brow_side_table} from "../images/brow_side_table_images.svg"
-export {default as stand_lamp} from "../images/stand_lamp_images.svg"
-export {default as pillow} from "../images/pillow_images.svg"
-export {default as lamp} from "../images/lamp_image.svg"
-export {default as basket} from "../images/basket_image.svg"
-export {default as white_pillow} from "../images/off_white_pillow_image.svg"
+export {default as sofaImages} from "../images/SofaImages.svg"
+export {default as luxuaryImages} from "../images/LuxuarySofaImages.svg"
+export {default as tableLamp} from "../images/TableLampImages.svg"
+export {default as cozySofa} from "../images/CozySofaImages.svg"
+export {default as drawer} from "../images/DrawerImages.svg"
+export {default as brow_side_table} from "../images/BrowSideTableImages.svg"
+export {default as stand_lamp} from "../images/StandLampImages.svg"
+export {default as pillow} from "../images/PillowImages.svg"
+export {default as lamp} from "../images/LampImage.svg"
+export {default as basket} from "../images/BasketImage.svg"
+export {default as white_pillow} from "../images/OffWhitePillowImage.svg"
 
 
 //saleUpData Images
 
-export {default as saleUp} from "../images/saleup_image.svg"
+export {default as saleUp} from "../images/SaleUpImages.svg"
 
 
 //cart Images
 
-export {default as cart_tablelamp} from "../images/cart_tablelamp.svg"
+export {default as cart_tablelamp} from "../images/CartTableLamp.svg"
 
 
 
